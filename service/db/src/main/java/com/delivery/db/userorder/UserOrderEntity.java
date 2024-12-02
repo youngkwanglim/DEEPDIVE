2250d1292d830b6ca6a842ce1fa346576b5ed455
youngkwanglim <richard08@naver.com>
2024-12-02 09:52:08 +0900

    12월 2일 수업 학습 내용

---
+++ service/db/src/main/java/com/delivery/db/userorder/UserOrderEntity.java
@@ -0,0 +1,44 @@
+package com.delivery.db.userorder;
+
+import com.delivery.db.BaseEntity;
+import com.delivery.db.userorder.enums.UserOrderStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@SuperBuilder
+@Entity
+@Table(name = "user_order")
+public class UserOrderEntity extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50, nullable = false)
+    private UserOrderStatus status;
+
+    @Column(precision = 11, scale = 4, nullable = false)
+    private BigDecimal amount;
+
+    private LocalDateTime orderedAt;
+
+    private LocalDateTime acceptedAt;
+
+    private LocalDateTime cookingStartedAt;
+
+    private LocalDateTime receivedAt;
+}
---
+++ service/db/src/main/java/com/delivery/db/userorder/UserOrderRepository.java
@@ -0,0 +1,7 @@
+package com.delivery.db.userorder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserOrderRepository extends JpaRepository<UserOrderEntity, Long> {
+
+}
